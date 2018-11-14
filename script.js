ae489ef800c12f4bb02c39017e1f1d479bf4560c
1 <test1@test.ru>
2018-11-14 20:36:27 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js">
+
+    window.addEventListener("load", function () {
+        const app = new Vue({
+            el: "#app",
+            data: {
+                str1: "",
+                str2: "",
+                result: "",
+                showPopup: false
+            },
+
+            methods: {
+                add: function () {
+                    let c = +this.str1+ +this.str2;
+                    this.result = "Ответ: " + c;
+
+                    if (this.str1 === "" || this.str2 === "") {
+                        app.showPopup = true;
+                        this.result="";
+                    }
+                },
+
+                sub: function () {
+                    let c = +this.str1 - +this.str2;
+                    this.result = "Ответ: " + c;
+
+                    if (this.str1 === "" || this.str2 === "") {
+                        app.showPopup = true;
+                        this.result="";
+                    }
+                },
+
+                multi: function () {
+                    let c = +this.str1 * +this.str2;
+                    this.result = "Ответ: " + c;
+
+                    if (this.str1 === "" || this.str2 === "") {
+                        app.showPopup = true;
+                        this.result="";
+                    }
+                },
+                div: function () {
+                    let c = +this.str1 / +this.str2;
+                    this.result = "Ответ: " + c;
+
+                    if (this.str1 === "" || this.str2 === "") {
+                        app.showPopup = true;
+                        this.result="";
+                    }
+                },
+                change: function () {
+                    this.str1 = "";
+                    this.str2 = "";
+                    this.result = "";
+                    app.showPopup = false;
+
+                }
+            }
+        });
+    });
